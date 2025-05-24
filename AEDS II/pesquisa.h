8432ad3efa278ac21d62a66b75237868ad369c04
RafaelBrantNobre <rafael.rbn.gb@gmail.com>
2025-05-24 12:28:01 -0300

    Delete AEDS II directory

--- AEDS II/pesquisa.h
+++
@@ -1,9 +0,0 @@
-#ifndef PESQUISA_H
-#define PESQUISA_H
-
-#include "tarefa.h"
-
-// Função de busca binária
-Tarefa *buscar_tarefa_por_id(Tarefa *tarefas[], int size, int id);
-
-#endif // PESQUISA_H
